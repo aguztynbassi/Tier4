fc28bcf3f32be685972ee8d12a7f803edc103254
aguztynbassi <agustin.bassi.dellelce@gmail.com>
2023-12-07 23:04:56 -0300

    ordeno los archivos

--- APP/app.py
+++ APP/app.py
@@ -1,3 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  app.py
+#  
+#  Copyright 2022 Agustin Bassi <[AT]aguztynbassi>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+
 from flask import Flask, render_template
 import json
 from prettytable import PrettyTable
@@ -11,18 +34,16 @@ def load_json(file_path):
 
 def generate_table(data_list):
     table = PrettyTable()
-    table.field_names = ["ID", "Name", "Trigger Condition", "ATT&CK Category", "ATT&CK Tag", "ATT&CK ID", "Minimum Log Source Requirement", "Query"]
+    table.field_names = ["ID", "Name", "ATT&CK Category", "ATT&CK Tag", "ATT&CK ID", "Minimum Log Source Requirement"]
 
     for data in data_list:
         table.add_row([
             data["ID"],
             data["Name"],
-            data["Trigger_Condition"],
             ", ".join(data["ATT&CK_Category"]),
             ", ".join(data["ATT&CK_Tag"]),
             ", ".join(data["ATT&CK_ID"]),
-            ", ".join(data["Minimum_Log_Source_Requirement"]),
-            data["Query"]
+            ", ".join(data["Minimum_Log_Source_Requirement"])
         ])
 
     return table.get_html_string()
